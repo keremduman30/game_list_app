834b41a13c3919811bf5a7d5cc90073809c24229
keremduman30 <berwari30_1907@hotmail.com>
2023-10-25 22:46:25 +0300

    header improved

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import { useState } from "react";
 function App() {
+  const [theme, setTheme] = useState<boolean>(false);
   return (
-    <div className="App">
+    <div className={`${theme && "bg-[#121212]"} h-screen`}>
+      <Header toggle={theme} setToggle={setTheme} />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,38 @@
-import { useState } from "react";
 import logo from "../assets/Images/logo.png";
-import { HiMoon, HiSun, HiSearch } from "react-icons/hi";
+import { HiMoon, HiSun, HiOutlineSearch } from "react-icons/hi";
 
-const Header = () => {
-  const [toggle, settoggle] = useState<boolean>(false);
-  return <div></div>;
+type HeaderProps = {
+  toggle: boolean;
+  setToggle(toggle: boolean): void;
+};
+
+const Header = ({ toggle, setToggle }: HeaderProps) => {
+  return (
+    <div className="flex items-center p-3 ">
+      <img src={logo} width={60} height={60} />
+      <div className="flex items-center w-full bg-slate-200 p-4 mx-5 rounded-full">
+        <HiOutlineSearch className="text-xl text-slate-600" />
+        <input
+          type="text"
+          placeholder="Search Games"
+          className="w-full outline-none bg-transparent px-2 dark:bg-cyan-500 "
+        />
+      </div>
+      <div>
+        {toggle ? (
+          <HiMoon
+            onClick={() => setToggle(!toggle)}
+            className="text-[60px] bg-slate-200 text-black p-2 rounded-full cursor-pointer"
+          />
+        ) : (
+          <HiSun
+            onClick={() => setToggle(!toggle)}
+            className="text-[60px] bg-slate-200 text-black p-2 rounded-full cursor-pointer"
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
